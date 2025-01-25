d65b8b17976928375067ca901e67136aa1f647a2
shravanibendre <shravanibendre2002@gmail.com>
2025-01-25 15:56:39 +0530

    RENOTES
    This is my c program in array .

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+ int A[]={4,8,3,5,7};
+ printf("array elements are:\n");
+     int i;
+     for(i=0;i<5;i++)
+     { //i<5 2<5t 3<5t 4<5t 5<5false
+     	printf("%d\n",A[i] );
+	 }
+ 
+ 
+ 
+ 
+}
